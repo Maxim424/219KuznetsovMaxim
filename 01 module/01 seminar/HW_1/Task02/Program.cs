30e7023355f0a71f5e34131ddd4d39d3d08e88b7
MVKuzn <mvkuznetsov_3@edu.hse.ru>
2021-09-12 15:54:19 +0300

    Task01 & Task02

---
+++ 01 module/01 seminar/HW_1/Task02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("               ___             ____             _____   ____     ");
+            Console.WriteLine("|  /    \\  |     /   |    |   |       |   |    |     |  |   |   ");
+            Console.WriteLine("| /      \\ |    /    |    |   |       |   |    |     |  |   /   ");   
+            Console.WriteLine("|/        \\|   /     |____|   |____   |   |    |     |  |__/_   ");
+            Console.WriteLine("|\\         |   ____  |    |   |       |   |    |     |  |    \\ ");
+            Console.WriteLine("| \\        |      |  |    |   |       |   |    |     |  |     \\");
+            Console.WriteLine("|  \\       |      |  |    |   |       |   |    |     |  |      |");
+            Console.WriteLine("|   \\    __|   ___|  |    |   |____   |___|_   |_____|  |______|");
+            Console.WriteLine("                                           |                     ");
+        }
+    }
+}
