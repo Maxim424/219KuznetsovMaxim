b792c81acaab596b55c263c8a510b9a20e52eb0b
MVKuzn <mvkuznetsov_3@edu.hse.ru>
2021-09-12 19:05:39 +0300

    Task04 & Task05

---
+++ 01 module/01 seminar/HW_1/Task04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите U: ");
+            double U = double.Parse(Console.ReadLine());
+            Console.Write("Введите R: ");
+            double R = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("I = " + (U/R));
+            Console.WriteLine("P = " + ((U*U)/R));
+        }
+    }
+}
---
+++ 01 module/01 seminar/HW_1/Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину катета a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите длину катета b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Гипотенуза c = " + (Math.Pow(a*a+b*b, 0.5)));
+        }
+    }
+}
