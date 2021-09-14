fd05bc1f4cced92db05f36378696c4ed9054b309
MVKuzn <mvkuznetsov_3@edu.hse.ru>
2021-09-14 19:40:20 +0300

    CW_3 added

---
+++ 01 module/01 seminar/CW_3/Task01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task01
+{
+    class MainClass
+    {
+        public static bool Function(bool p, bool q) {
+            return !(p & q) & !(p | !q);
+        }
+
+        public static void Function2(bool p, bool q, out bool f)
+        {
+            f = !(p & q) & !(p | !q);
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("p    q   f");
+            Console.WriteLine("0    0   " + Function(false, false));
+            Console.WriteLine("0    1   " + Function(false, true));
+            Console.WriteLine("1    0   " + Function(true, false));
+            Console.WriteLine("1    1   " + Function(true, true));
+
+            bool f;
+            Console.WriteLine();
+            Console.WriteLine("p    q   f");
+            Function2(false, false, out f);
+            Console.WriteLine("0    0   " + f);
+            Function2(false, true, out f);
+            Console.WriteLine("0    1   " + f);
+            Function2(true, false, out f);
+            Console.WriteLine("1    0   " + f);
+            Function2(true, true, out f);
+            Console.WriteLine("1    1   " + f);
+        }
+    }
+}
---
+++ 01 module/01 seminar/CW_3/Task02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task02
+{
+    class MainClass
+    {
+        public static void F1() {
+            for(int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i*i);
+            }
+        }
+
+        public static void F2()
+        {
+            int i= 1;
+            while (i < 11)
+            {
+                Console.WriteLine(i*i);
+                i++;
+            }
+        }
+
+        public static void F3()
+        {
+            int i = 1;
+            do
+            {
+                Console.WriteLine(i*i);
+                i++;
+            } while (i < 11);
+        }
+
+
+        public static void Main(string[] args)
+        {
+            F1();
+            F2();
+            F3();
+        }
+    }
+}
