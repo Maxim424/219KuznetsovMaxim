6be89b957752a440afe8dd7c0d40e992d86e1809
MVKuzn <mvkuznetsov_3@edu.hse.ru>
2021-09-16 12:20:45 +0300

    HW_2 added

---
+++ 01 module/01 seminar/HW_2/Task01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task01
+{
+    class MainClass
+    {
+        public static double CalculateF(double x)
+        {
+            double F = -4;
+            double x2 = x * x;
+            double x3 = x2 * x;
+            double x4 = x3 * x;
+            for (int i = 0; i < 12; i++)
+            {
+                F += x4;
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                F += x3;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                F -= x2;
+            }
+            F = F + x + x;
+            return F;
+        }
+
+        public static void Main(string[] args)
+        {
+            double x;
+            bool flag;
+            do
+            {
+                Console.WriteLine("Введите x: ");
+                flag = double.TryParse(Console.ReadLine(), out x);
+                double F = CalculateF(x);
+                if (flag) {
+                    Console.WriteLine("F(x) = " + F);
+                }
+
+            } while (!flag);
+        }
+    }
+}
---
+++ 01 module/01 seminar/HW_2/Task02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task02
+{
+    class MainClass
+    {
+        public static void FindMax(int P) {
+            int n1 = P % 10;
+            int n2 = (P / 10) % 10;
+            int n3 = (P / 100) % 10;
+            int res1 = n1 * 100 + n2 * 10 + n3;
+            int res2 = n1 * 100 + n3 * 10 + n2;
+            int res3 = n2 * 100 + n1 * 10 + n3;
+            int res4 = n2 * 100 + n3 * 10 + n1;
+            int res5 = n3 * 100 + n1 * 10 + n2;
+            int res6 = n3 * 100 + n2 * 10 + n1;
+
+            Console.WriteLine(Math.Max(res1, Math.Max(res2, Math.Max(res3, Math.Max(res4, Math.Max(res5, res6))))));
+        }
+
+        public static void Main(string[] args)
+        {
+            int P;
+            bool flag;
+            do
+            {
+                flag = int.TryParse(Console.ReadLine(), out P);
+            } while (!flag && Convert.ToString(P).Length==3 && P>0);
+            FindMax(P);
+        }
+    }
+}
