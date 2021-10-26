498f70438aee2016e881546d5a2297a328580215
MVKuzn <mvkuznetsov_3@edu.hse.ru>
2021-10-26 19:33:48 +0300

    CW_1 added

---
+++ 02 module/CW_1/Task01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegularPolygon regularPolygon = new RegularPolygon();
+            regularPolygon.Radius = 20;
+            regularPolygon.SidesNumber = 5;
+            Console.WriteLine(regularPolygon.PolygonData());
+
+            Console.WriteLine("Введите количество элементов массива");
+            int n = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            RegularPolygon[] regularPolygons = new RegularPolygon[n];
+            for (int i = 0; i < n; i++)
+            {
+                regularPolygons[i] = new RegularPolygon(random.Next(1, 10), random.Next(1, 10));
+                Console.WriteLine(regularPolygons[i].PolygonData());
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 02 module/CW_1/Task01/RegularPolygon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Task01
+{
+    public class RegularPolygon
+    {
+        private int sidesNumber;
+
+
+        private double radius;
+
+        public RegularPolygon(int sidesNumber, double radius)
+        {
+            SidesNumber = sidesNumber;
+            Radius = radius;
+        }
+
+        public RegularPolygon()
+        {
+            SidesNumber = 3;
+            Radius = 10;
+        }
+
+        public string PolygonData()
+        {
+            double a = 2 * Radius * Math.Tan(Math.PI / (double)SidesNumber);
+            Perimeter = a * SidesNumber;
+            Area = SidesNumber * Math.Pow(Radius, 2) * Math.Tan(Math.PI / (double)SidesNumber);
+            string result = "SidesNumber: " + SidesNumber + "\n" + "Radius: " + Radius + "\n" + "Perimeter: " + Perimeter + "\n" + "Area: " + Area + "\n";
+            return result;
+        }
+
+        public int SidesNumber { get => sidesNumber; set => sidesNumber = value; }
+        public double Radius { get => radius; set => radius = value; }
+        public double Perimeter { get; set; }
+        public double Area { get; set; }
+    }
+}
