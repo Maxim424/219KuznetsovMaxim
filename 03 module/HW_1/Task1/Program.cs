1dfd6c9d236b565a73fab63cc52ca41dd72b3e4e
MVKuzn <mvkuznetsov_3@edu.hse.ru>
2022-01-13 21:48:16 +0300

    HW_1 added

---
+++ 03 module/HW_1/Task1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        delegate int Cast(double x);
+
+        public static int ToTheNearestEven(double x)
+        {
+            int t = (int)x;
+            if (t % 2 == 0)
+            {
+                return t;
+            }
+            else
+            {
+                return t + 1;
+            }
+        }
+
+        public static int CalculateExponent(double x)
+        {
+            int t = (int)x;
+            return t.ToString().Length - 1;
+        }
+
+        static void Main(string[] args)
+        {
+            Cast cast1 = delegate (double x)
+            {
+                int t = (int)x;
+                if (t%2==0)
+                {
+                    return t;
+                }
+                else
+                {
+                    return t + 1;
+                }
+            };
+            Cast cast2 = delegate (double x)
+            {
+                int t = (int)x;
+                return t.ToString().Length - 1;
+            };
+            Console.WriteLine("Введите вещественное число:");
+            double num = double.Parse(Console.ReadLine());
+            Console.WriteLine($"cast1: {cast1(num)}");
+            Console.WriteLine($"cast2: {cast2(num)}");
+
+            Cast cast = cast1;
+            cast += cast2;
+            Console.WriteLine($"cast: {cast(num)}");
+
+            Cast lambdaCast = x => (int)x + (int)x % 2;
+            Console.WriteLine($"lambdaCast1: {lambdaCast.Invoke(num)}");
+            lambdaCast += x => ((int)x).ToString().Length - 1;
+            Console.WriteLine($"lambdaCast2: {lambdaCast.Invoke(num)}");
+            lambdaCast -= x => ((int)x).ToString().Length - 1;
+            Console.WriteLine($"lambdaCast: {lambdaCast.Invoke(num)}");
+
+            Cast MethodCast = ToTheNearestEven;
+            Console.WriteLine($"MethodCast1: {MethodCast.Invoke(num)}");
+            MethodCast += CalculateExponent;
+            Console.WriteLine($"MethodCast2: {MethodCast.Invoke(num)}");
+            MethodCast -= CalculateExponent;
+            Console.WriteLine($"MethodCast: {MethodCast.Invoke(num)}");
+        }
+    }
+}
---
+++ 03 module/HW_1/Task3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Task3
+{
+    
+
+    class TemperatureConverterImp
+    {
+        public double ToCelsius(double x)
+        {
+            return (5.0 / 9.0) * (x - 32);
+        }
+
+        public double ToFahrenheit(double x)
+        {
+            return (9.0 / 5.0) * x + 32;
+        }
+    }
+
+    static class StaticTempConverters
+    {
+        public static double CelsiusToKelvin(double x)
+        {
+            return x + 273.15;
+        }
+
+        public static double CelsiusToReaumur(double x)
+        {
+            return (4.0/5.0) * x;
+        }
+
+        public static double CelsiusToRankin(double x)
+        {
+            return (x + 273.15) * (9.0 / 5.0);
+        }
+
+        public static double RankinToCelsius(double x)
+        {
+            return (x - 491.67) * (5.0 / 9.0);
+        }
+
+        public static double ReaumurToCelsius(double x)
+        {
+            return x * (5.0 / 4.0);
+        }
+
+        public static double KelvinToCelsius(double x)
+        {
+            return x - 273.15;
+        }
+
+    }
+
+    class Program
+    {
+        public delegate double DelegateConvertTemperature(double x);
+
+        static void Main(string[] args)
+        {
+            TemperatureConverterImp temperatureConverter = new TemperatureConverterImp();
+            DelegateConvertTemperature delegateConvert1 = temperatureConverter.ToCelsius;
+            DelegateConvertTemperature delegateConvert2 = temperatureConverter.ToFahrenheit;
+            DelegateConvertTemperature[] array = new DelegateConvertTemperature[2];
+            array[0] = delegateConvert1;
+            array[1] = delegateConvert2;
+            double temperature = double.Parse(Console.ReadLine());
+            Console.WriteLine(array[0].Invoke(temperature));
+            Console.WriteLine(array[1].Invoke(temperature));
+
+            DelegateConvertTemperature[] array2 = new DelegateConvertTemperature[3];
+            array2[0] = StaticTempConverters.CelsiusToKelvin;
+            array2[1] = StaticTempConverters.CelsiusToRankin;
+            array2[2] = StaticTempConverters.CelsiusToReaumur;
+
+            temperature = double.Parse(Console.ReadLine());
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine($"{array2[i].Method}: {array2[i].Invoke(temperature)}");
+            }
+        }
+    }
+}
