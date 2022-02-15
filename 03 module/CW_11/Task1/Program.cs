7e9010f428b4cfedcf293fb8ed71e9c1f65695a6
MVKuzn <mvkuznetsov_3@edu.hse.ru>
2022-02-15 19:34:04 +0300

    CW_11 added

---
+++ 03 module/CW_11/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo fi = new FileInfo("1.txt");
+            if (fi.Exists)
+            {
+                byte[] array;
+                using (FileStream fs = new FileStream("1.txt", FileMode.Open))
+                {
+                    array = new byte[fs.Length];
+                    fs.Read(array, 0, array.Length);
+                    foreach (var item in array)
+                    {
+                        Console.Write((char)item);
+                    }
+                }
+                using (FileStream fs = new FileStream("1.txt", FileMode.Append))
+                {
+                    byte[] array2 = {(byte)(array[array.Length-1]+1)};
+                    fs.Write( array2, 0, array2.Length);
+                }
+            }
+            else
+            {
+                using (FileStream fs = new FileStream("1.txt", FileMode.Create))
+                {
+                    byte[] array = System.Text.Encoding.Default.GetBytes("A");
+                    fs.Write( array, 0, array.Length);
+                }
+            }
+        }
+    }
+}
---
+++ 03 module/CW_11/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BinaryWriter bw = new BinaryWriter(new FileStream("Numbers.txt", FileMode.OpenOrCreate)))
+            {
+                Random random = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    bw.Write(random.Next(1, 101));
+                }
+            }
+
+            int[] array = new int[10];
+            using (BinaryReader br = new BinaryReader(new FileStream("Numbers.txt", FileMode.Open)))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    array[i] = br.ReadInt32();
+                }
+            }
+            Array.ForEach(array, x=>Console.Write(x + " "));
+        }
+    }
+}
