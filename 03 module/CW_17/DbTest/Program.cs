7090d495b1844e92de0ab40225dafed0dfa1dabb
MVKuzn <mvkuznetsov_3@edu.hse.ru>
2022-03-25 23:05:54 +0300

    HW_14 added

---
+++ 03 module/CW_17/DbTest/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace DbTest
+{
+    class Program
+    {
+        private static DataTable ExecuteSQL_DataTable(string connectionString, string SQL,
+            params Tuple<string, string>[] parameters)
+        {
+            DataTable dt = new DataTable();
+            using (SqliteConnection con = new SqliteConnection(connectionString))
+            {
+                using (SqliteCommand cmd = new SqliteCommand(SQL, con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    foreach (var tuple in parameters)
+                        cmd.Parameters.Add(new SqliteParameter(tuple.Item1, tuple.Item2));
+                    con.Open();
+                    SqliteDataReader reader = cmd.ExecuteReader();
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        dt.Columns.Add(new DataColumn(reader.GetName(i)));
+
+                    int rowIndex = 0;
+                    while (reader.Read())
+                    {
+                        DataRow row = dt.NewRow();
+                        dt.Rows.Add(row);
+                        for (int i = 0; i < reader.FieldCount; i++)
+                            dt.Rows[rowIndex][i] = (reader.GetValue(i));
+                        rowIndex++;
+                    }
+                }
+            }
+
+            return dt;
+        }
+
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=AdventureWorksLT.db";
+            string SQL = "SELECT * FROM Product";
+            string SQL2 = "SELECT * FROM Product WHERE ProductID = 680";
+            string SQL3 = "SELECT * FROM Product WHERE ListPrice > 680 and ListPrice < 1000";
+            string SQL4 = "SELECT * FROM Product WHERE ProductID = 1";
+
+            SqliteConnection con = new SqliteConnection(connectionString);
+            con.Open();
+            SqliteCommand command = new SqliteCommand();
+            command.Connection = con;
+            command.CommandText = "UPDATE Product SET StandardCost='100' WHERE ProductID='680'";
+            command.ExecuteNonQuery();
+            command.CommandText = "UPDATE Product SET ListPrice='100' WHERE ProductID='680'";
+            command.ExecuteNonQuery();
+            command.CommandText =
+                "INSERT INTO Product (ProductID, Name, ProductNumber, StandardCost, ListPrice,  SellStartDate, rowguid) VALUES (10000000, 'NewProduct1', 'A1', 0, 0, '2022-06-01 00:00:00', '011')";
+            command.ExecuteNonQuery();
+            command.CommandText = "DELETE FROM Product WHERE ProductID='10000000'";
+            command.ExecuteNonQuery();
+            DataTable dt = ExecuteSQL_DataTable(connectionString, SQL4);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                foreach (var t in row.ItemArray)
+                    Console.Write(t + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 03 module/HW_14/HW_14/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Threading.Channels;
+using System.Linq;
+
+namespace HW_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> collection = new List<int>();
+            Console.WriteLine("Введите размер коллекции");
+            int n = int.Parse(Console.ReadLine());
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                collection.Add(rnd.Next(-1000, 1001));
+            }
+
+            Console.WriteLine("Изначальная коллекция");
+            collection.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine("\nКоллекция квадратов");
+            var z1 = from elem in collection
+                select elem * elem;
+            foreach (var item in z1)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\nКоллекция положительных двузначных чисел");
+            var z2 = from elem in collection
+                where elem>0 && elem.ToString().Length==2
+                select elem;
+            foreach (var item in z2)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nКоллекция чётных чисел с сортировкой по убыванию");
+            var z3 = from elem in collection
+                where elem%2==0
+                orderby elem descending 
+                select elem;
+            foreach (var item in z3)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\nКоллекция с группировкой по порядку");
+            var z4 = from elem in collection
+                group elem by Math.Abs(elem).ToString().Length
+                into newCollection
+                select newCollection;
+            foreach (var item in z4)
+            {
+                Console.WriteLine(item.Key);
+                foreach (var num in item)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
